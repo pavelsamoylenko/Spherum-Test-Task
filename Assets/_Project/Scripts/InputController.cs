c78654b9ee2113eac71f1132fffd646b4117f062
pavelsamoylenko <46072520+pavelsamoylenko@users.noreply.github.com>
2024-01-22 13:41:55 +0300

    better input

--- Assets/_Project/Scripts/InputController.cs
+++ Assets/_Project/Scripts/InputController.cs
@@ -14,26 +14,23 @@ namespace _Project.Scripts
         private void Awake() {
             Observable
                 .EveryUpdate()
-                .Subscribe(_ => CheckInput())
+                .Subscribe(_ => PollInput())
                 .AddTo(this);
         }
 
-        private void CheckInput()
+        private void PollInput()
         {
             Vector3 forward = GetCameraForward();
             Vector3 right = GetCameraRight();
-        
-            if (Input.GetKey(KeyCode.W)) redCubeController.Move(forward * speed);
-            if (Input.GetKey(KeyCode.A)) redCubeController.Move(-right * speed);
-            if (Input.GetKey(KeyCode.S)) redCubeController.Move(-forward * speed);
-            if (Input.GetKey(KeyCode.D)) redCubeController.Move(right * speed);
-
-            if (Input.GetKey(KeyCode.UpArrow)) greenCubeController.Move(forward * speed);
-            if (Input.GetKey(KeyCode.LeftArrow)) greenCubeController.Move(-right * speed);
-            if (Input.GetKey(KeyCode.DownArrow)) greenCubeController.Move(-forward * speed);
-            if (Input.GetKey(KeyCode.RightArrow)) greenCubeController.Move(right * speed);
+            
+            redCubeController.Move(forward * Input.GetAxis("Vertical2") * speed);
+            redCubeController.Move(right * Input.GetAxis("Horizontal2") * speed);
+            
+            greenCubeController.Move(forward * Input.GetAxis("Vertical") * speed);
+            greenCubeController.Move(right * Input.GetAxis("Horizontal") * speed);
         }
 
+        
         private Vector3 GetCameraForward()
         {
             Vector3 forward = cam.transform.forward;
